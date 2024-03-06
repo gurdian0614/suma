215018030257969b8a70a05631668437e90d60d1
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-05 19:15:48 -0600

    Creando funcion de alerta

---
+++ src/functions.js
@@ -0,0 +1,18 @@
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+export function alerta(mensaje, icono, id = '') {
+    onFocus(id);
+    const MySwal = withReactContent(Swal);
+
+    MySwal.fire({
+        title: mensaje,
+        icon: icono
+    });
+}
+
+function onFocus(id) {
+    if (id !== '') {
+        document.getElementById(id).focus();
+    }
+}

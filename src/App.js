64b08a0a2268f20a88836abb12c40aae1cc65ffb
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-05 19:15:19 -0600

    Agregando componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Suma from './components/Suma';
 
 function App() {
   return (
     <div className="App">
-      
+      <Suma />
     </div>
   );
 }
